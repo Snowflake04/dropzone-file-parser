dabbff68ecfbea80f2c19ddc08dea380cc85483b
Geevargehse A K <geevarghese@fileago.com>
2025-02-26 00:12:19 +0530

    added script to remove scripts

--- scripts/cleanup.cjs
+++ scripts/cleanup.cjs
@@ -10,6 +10,9 @@ const packageData = require(packagePath);
 // Remove the devDependencies field
 delete packageData.devDependencies;
 
+// Remove scripts field
+delete packageData.scripts;
+
 // Write the updated package.json back to the file
 fs.writeFileSync(packagePath, JSON.stringify(packageData, null, 2));
 

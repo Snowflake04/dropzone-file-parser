844247627e6d638bc8b2b8f8bd9cf59f980e5586
Geevargehse A K <geevarghese@fileago.com>
2025-02-26 00:15:10 +0530

    chore:fixed scripts

--- scripts/cleanup.cjs
+++ scripts/cleanup.cjs
@@ -10,9 +10,6 @@ const packageData = require(packagePath);
 // Remove the devDependencies field
 delete packageData.devDependencies;
 
-// Remove scripts field
-delete packageData.scripts;
-
 // Write the updated package.json back to the file
 fs.writeFileSync(packagePath, JSON.stringify(packageData, null, 2));
 
